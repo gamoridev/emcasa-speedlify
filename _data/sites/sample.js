d251eef240689a8715893cf75cb0ccdc04748cc0
gamoridev <gamoridev@protonmail.com>
2022-12-16 16:45:29 -0300

    test url

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,10 +1,11 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
-	description: "The default sites that get tested",
-	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
-	},
-	urls: [
-		"https://www.speedlify.dev/"
-	]
+  name: "Sample", // optional, falls back to object key
+  description: "The default sites that get tested",
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: [
+    "https://www.speedlify.dev/",
+    "https://emcasa.com/imoveis/sp/sao-paulo?nextView=true",
+  ],
 };
